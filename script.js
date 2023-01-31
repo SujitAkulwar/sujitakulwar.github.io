b686f0cfd4baee3543fd44a925fa30979da00f59
sujit akulwar <85801946+SujitAkulwar@users.noreply.github.com>
2023-02-01 00:00:54 +0530

    1

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-$('body').ripples({
-	resolution: 700,
-	dropRadius: 20,
-	perturbance: 0.01,
-});
 
-const counter = 0;
-var music = document.getElementById("music");
-music.pause();
-function play(){
-    music.play();
-}
-function pause(){
-	music.pause();
-}
