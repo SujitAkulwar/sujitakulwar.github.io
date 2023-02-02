1ac553ecc782f65a6564a880dccfe94694f09552
sujit akulwar <85801946+SujitAkulwar@users.noreply.github.com>
2023-02-02 14:43:28 +0530

    complete ig

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+function home() {
+    var home = document.getElementById("img");
+    home.scrollIntoView(); 
+}
+
+function about() {
+    var about = document.getElementById("about");
+    about.scrollIntoView(); 
+}
+
+function pro() {
+    var pro = document.getElementById("ptitle");
+    pro.scrollIntoView();
+}
